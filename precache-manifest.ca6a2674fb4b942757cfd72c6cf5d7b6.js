cadef16b77f014bd52367fa668f32c5d55331b56
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2019-06-20 22:58:10 -0400

    Updates

---
+++ precache-manifest.ca6a2674fb4b942757cfd72c6cf5d7b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac33a7e5cf8b3c3fe6ec4075feaedad",
+    "url": "/porfolio-react/index.html"
+  },
+  {
+    "revision": "d08f01e3a9355c51ce2f",
+    "url": "/porfolio-react/static/css/main.c9c036cb.chunk.css"
+  },
+  {
+    "revision": "2ac6d0b51cf2208b3ae3",
+    "url": "/porfolio-react/static/js/2.47cab030.chunk.js"
+  },
+  {
+    "revision": "d08f01e3a9355c51ce2f",
+    "url": "/porfolio-react/static/js/main.bb7cc532.chunk.js"
+  },
+  {
+    "revision": "37b5f64c06b97c09634b",
+    "url": "/porfolio-react/static/js/runtime~main.bf82f86d.js"
+  },
+  {
+    "revision": "666f863b8e07356e079e1c57c7653d8f",
+    "url": "/porfolio-react/static/media/face-shot.666f863b.jfif"
+  },
+  {
+    "revision": "d30a30c46771607bdf38b3887c56780b",
+    "url": "/porfolio-react/static/media/website1.d30a30c4.png"
+  },
+  {
+    "revision": "e0889917b4de0136d75cf8a0fedb3dfd",
+    "url": "/porfolio-react/static/media/website2.e0889917.png"
+  },
+  {
+    "revision": "1f07e659d4c521fcf583ca3eba87fe0e",
+    "url": "/porfolio-react/static/media/website3.1f07e659.png"
+  },
+  {
+    "revision": "3c2a6e907296b5d65568b822de55c0f5",
+    "url": "/porfolio-react/static/media/website4.3c2a6e90.png"
+  }
+]);
